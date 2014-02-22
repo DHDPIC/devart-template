a7608b4d4288df2ba4ef74b536386b6b2ef1f5a7
Alex Prokop <alex@baseten.co.uk>
2014-02-22 23:30:47 +0000

    changed port number back to 3000

--- project_code/app.js
+++ project_code/app.js
@@ -3,7 +3,7 @@ var app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(1337);
+server.listen(3000);
 
 app.use(express.static(__dirname + '/public'));
 
